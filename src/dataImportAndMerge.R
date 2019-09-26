5f9034e6d1a89a2659f05c332290981c07615ea8
Hendrik Knoche <hendrik.knoche@gmail.com>
2019-09-26 13:51:28 +0200

    link up to group g-sheets

---
+++ src/dataImportAndMerge.R
@@ -0,0 +1,5 @@
+
+sotaOverview<-gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1aocnRPj-2Rhv8bBj7nsG0pHMqVI1H1dWXRvL-5SfYRc/edit#gid=0')
+pubs<-gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1aocnRPj-2Rhv8bBj7nsG0pHMqVI1H1dWXRvL-5SfYRc/edit#gid=2120717860')
+outOfViewanglesPerStudy<-gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1aocnRPj-2Rhv8bBj7nsG0pHMqVI1H1dWXRvL-5SfYRc/edit#gid=1918185269')
+tasks<-gsheet::gsheet2tbl('https://docs.google.com/spreadsheets/d/1aocnRPj-2Rhv8bBj7nsG0pHMqVI1H1dWXRvL-5SfYRc/edit#gid=2009986896')
